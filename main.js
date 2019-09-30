286aeb9215f723afc6ced4bd4a7f8812926535f5
Brandon Wilburn <brandon.wilburn@wustl.edu>
2019-09-30 00:05:59 -0500

    Sunday Push

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var pathEls = document.querySelectorAll('path');
-console.log(pathEls);
+// console.log(pathEls);
 
 for (var i = 0; i < pathEls.length; i++) {
   var pathEl = pathEls[i];
@@ -11,19 +11,17 @@ for (var i = 0; i < pathEls.length; i++) {
   // var array = anime.setStrokedasharray(pathEl);
   // pathEl.setAttribute('stroke-dashoffset', length/2);
 
-  // var offsetLength = Math.round(Math.random() * 2);
-  // if (offsetLength==0) {
-  //   oL = oL/2;
-  //   // offsetLength=1;
-  // } else if (offsetLength==1) {
-  //   oL = oL/8
-  //   // offsetLength=2;
-  // } else {
-  //   oL = oL/16
-  //   // offsetLength=3;
-  // }
+  var offsetLength = Math.round(Math.random() * 1);
+  if (offsetLength==0) {
+    oL = oL/2;
+    // offsetLength=1;
+  } else {
+    oL = oL/4
+    // offsetLength=2;
+  }
+  // console.log(offsetLength);
 
-  oL = oL/4;
+  // oL = oL/4;
 
   var colorR = Math.round(Math.random() * 2);
    if (colorR==0) {
@@ -40,7 +38,7 @@ for (var i = 0; i < pathEls.length; i++) {
   pathEl.setAttribute('stroke-dasharray', oL);
   pathEl.setAttribute('stroke', cR);
 
-  anime({
+  var tron = anime({
     targets: pathEl,
     strokeDashoffset: [0, length],
     // strokeDasharray: [array, 0],
@@ -51,7 +49,51 @@ for (var i = 0; i < pathEls.length; i++) {
     loop: true,
     direction: 'linear',
     easing: 'easeOutQuint',
-    autoplay: true
+    autoplay: false
   });
-
 }
+
+// function yell() {
+//   console.log("HOVERED");
+// }
+
+// var svgs = document.querySelectorAll('.svgContainer');
+
+// for (var i = 0; i < imgs.length; i++) {
+//   svgs[i].onclick = toggleAnimation;
+//   svgs[i].style.webkitAnimationPlayState = 'running';
+// }
+
+// function toggleAnimation() {
+//   var style;
+//   for (var i = 0; i < imgs.length; i++) {
+//     style = svg[i].style;
+//     if (style.webkitAnimationPlayState === 'running') {
+//       style.webkitAnimationPlayState = 'paused';
+//       document.body.className = 'paused';
+//     } else {
+//       style.webkitAnimationPlayState = 'running';
+//       document.body.className = '';
+//     }
+//   }
+// }
+
+document.querySelector('#controls .play').onclick = function playEm() {
+  for (var i = 0; i < pathEls.length; i++) {
+    var pathEl = pathEls[i];
+    tron.play;
+    console.log(pathEls[i]);
+
+  }
+}
+
+document.querySelector('#controls .pause').onclick = function pauseEm() {
+  for (var i = 0; i < pathEls.length; i++) {
+   pathEls[i].tron.pause;
+  }
+}
+  // document.querySelectorAll('.svgContainer').onmouseover = yell();
+
+  // document.querySelectorAll('#svgContainer').onmouseover = tron.play;
+  // document.querySelectorAll('svg').onmouseover = tron.pause;
+
